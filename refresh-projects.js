10e0799369f3efb3a3930a685b492e3aa5a21acb
snakeful <fulblind@gmail.com>
2022-04-20 18:39:16 -0600

    :art: add if folder is git repo

--- refresh-projects.js
+++ refresh-projects.js
@@ -2,6 +2,10 @@ const fs = require('fs');
 const child_process = require('child_process');
 const path = require('path');
 
+function isGitDir(dir) {
+  return fs.existsSync(path.join(dir, '.git'));
+}
+
 function getDirectories(dir) {
   return fs.readdirSync(dir).filter((file) => {
     const dirPath = fs.statSync(path.join(dir, file));
@@ -13,13 +17,15 @@ let [, , absoluteDir = __dirname] = process.argv;
 const dirs = getDirectories(absoluteDir);
 dirs.forEach((dir) => {
   const actualDir = path.join(absoluteDir, dir);
-  process.chdir(actualDir);
-  child_process.exec(`git pull`, { encoding: 'utf-8' },
-    (error, stdout, stderr) => {
-      if (error) {
-        console.log(actualDir, '-->', stderr.replaceAll('\n', ''));
-      } else {
-        console.log(actualDir, '-->', stdout.replaceAll('\n', ''));
-      }
-    });
+  if (isGitDir(actualDir)) {
+    process.chdir(actualDir);
+    child_process.exec(`git pull`, { encoding: 'utf-8' },
+      (error, stdout, stderr) => {
+        if (error) {
+          console.log(actualDir, '-->', stderr.replaceAll('\n', ''));
+        } else {
+          console.log(actualDir, '-->', stdout.replaceAll('\n', ''));
+        }
+      });
+  }
 });
